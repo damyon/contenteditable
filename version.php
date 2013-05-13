80d36ff00aec35f84cd03ed989fa008be1217e21
Damyon Wiese <damyon.wiese@gmail.com>
2013-05-13 23:31:41 +0800

    1: Set the version and maturity for uploading to plugins database

--- version.php
+++ version.php
@@ -28,3 +28,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2013050100;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013050100;        // Requires this Moodle version
 $plugin->component = 'editor_contenteditable';  // Full name of the plugin (used for diagnostics)
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->release  = 2013051300;
